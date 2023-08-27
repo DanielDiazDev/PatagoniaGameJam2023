1f99650e33a4df9350d3e32175a830dd0e95d462
EnzoMaurelli98 <enzomaurelli98@gmail.com>
2023-08-27 19:19:32 -0300

    Recuperando escena Game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,9 +10,7 @@ public class GameManager : MonoBehaviour
     private bool _puzzleCompleted = false;
     public static GameManager Instance { get; private set; }
 
-    public string music = "event:/MusicEvents/GameplayMusic/play_gameplay_music_112bpm";
-    FMOD.Studio.EventInstance musicEvent;
-
+   
     private void Awake()
     {
         if (Instance == null)
@@ -28,8 +26,7 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         ActivatePuzzle(_currentPuzzleIndex);
-        musicEvent = FMODUnity.RuntimeManager.CreateInstance(music);
-        musicEvent.start();
+       
     }
 
     private void Update()
@@ -44,7 +41,7 @@ public class GameManager : MonoBehaviour
             {
                 // Todos los rompecabezas han sido completados, cambia de escena
                 SceneManager.LoadScene("End");
-                musicEvent.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+                
             }
             else
             {
@@ -56,7 +53,7 @@ public class GameManager : MonoBehaviour
     public void CompletePuzzle()
     {
         _puzzleCompleted = true;
-        FMODUnity.RuntimeManager.PlayOneShot("event:/SFX Events/LevelComplete");
+    
     }
 
     private void ActivatePuzzle(int index)
