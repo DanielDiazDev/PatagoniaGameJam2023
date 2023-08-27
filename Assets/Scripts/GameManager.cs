8bb40b0642fcd1caa0c17c3b6f7683de1b57f13b
EnzoMaurelli98 <enzomaurelli98@gmail.com>
2023-08-27 17:43:06 -0300

    Mùsica Gameplay y LevelComplete

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,6 +10,9 @@ public class GameManager : MonoBehaviour
     private bool _puzzleCompleted = false;
     public static GameManager Instance { get; private set; }
 
+    public string music = "event:/MusicEvents/GameplayMusic/play_gameplay_music_112bpm";
+    FMOD.Studio.EventInstance musicEvent;
+
     private void Awake()
     {
         if (Instance == null)
@@ -25,6 +28,8 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         ActivatePuzzle(_currentPuzzleIndex);
+        musicEvent = FMODUnity.RuntimeManager.CreateInstance(music);
+        musicEvent.start();
     }
 
     private void Update()
@@ -39,6 +44,7 @@ public class GameManager : MonoBehaviour
             {
                 // Todos los rompecabezas han sido completados, cambia de escena
                 SceneManager.LoadScene("End");
+                musicEvent.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
             }
             else
             {
@@ -50,6 +56,7 @@ public class GameManager : MonoBehaviour
     public void CompletePuzzle()
     {
         _puzzleCompleted = true;
+        FMODUnity.RuntimeManager.PlayOneShot("event:/SFX Events/LevelComplete");
     }
 
     private void ActivatePuzzle(int index)
