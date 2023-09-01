998ffbccd4da17d638175e5be1afc1425bd8ee10
DanielDiazDev <danieldiazdev00@gmail.com>
2023-09-01 10:55:35 -0300

    Fixing Music puzzles

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,8 +8,10 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private Board[] _puzzleBoards;
     [SerializeField] private int _currentPuzzleIndex = 0;
-    [SerializeField] private string[] _musics;
+   // [SerializeField] private string _music;
     [SerializeField] private GameObject _gameObjectPaz;
+    //[SerializeField] private float _musicLayer1;
+    //[SerializeField] private float _musicLayer2;
     private bool _puzzleCompleted = false;
     private FMOD.Studio.EventInstance _instanceFmod;
     private bool _permitirMensajePaz = true;
@@ -17,9 +19,11 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-       _instanceFmod = FMODUnity.RuntimeManager.CreateInstance($"event:/{_musics[_currentPuzzleIndex]}");
+       _instanceFmod = FMODUnity.RuntimeManager.CreateInstance("event:/MusicEvents/GameplayMusic/play_gameplay_music_112bpm");
         ActivatePuzzle(_currentPuzzleIndex);
-       
+        _instanceFmod.start();
+
+
     }
 
     private void Update()
@@ -41,7 +45,7 @@ public class GameManager : MonoBehaviour
             _puzzleCompleted = false;
 
             DeactivatePuzzle(_currentPuzzleIndex);
-            _currentPuzzleIndex = (_currentPuzzleIndex + 1) % _puzzleBoards.Length; //Poner nueva variable de ultimoindex
+            _currentPuzzleIndex = (_currentPuzzleIndex + 1) % _puzzleBoards.Length; 
             if (_currentPuzzleIndex == 0)
             {
                 // Todos los rompecabezas han sido completados, cambia de escena
@@ -68,12 +72,47 @@ public class GameManager : MonoBehaviour
             puzzleBoard.gameObject.SetActive(false);
         }
 
-        // Activa solo el rompecabezas actual
         _puzzleBoards[index].gameObject.SetActive(true);
 
-        // FMODUnity.RuntimeManager.PlayOneShot($"event:/MusicEvents/{_musics[index]}");
-        _instanceFmod.start();
 
+        var musicLayer1 = 0.0f;
+        var musicLayer2 = 0.0f;
+        var musicLayer3 = 0.0f;
+        switch (index)
+        {
+            case 0:
+                musicLayer1 = 1;
+                musicLayer2 = 1;
+                musicLayer3 = 1;
+                break;
+            case 1:
+                musicLayer1 = 2;
+                musicLayer2 = 1;
+                musicLayer3 = 1;
+                break;
+            case 2:
+                musicLayer1 = 3;
+                musicLayer2 = 1;
+                musicLayer3 = 1;
+                break;
+            case 3:
+                musicLayer1 = 1;
+                musicLayer2 = 1;
+                musicLayer3 = 2;
+                break;
+            case 4:
+                musicLayer1 = 1;
+                musicLayer2 = 2;
+                musicLayer3 = 2;
+                break;
+            case 5:
+                musicLayer1 = 1;
+                musicLayer2 = 3;
+                musicLayer3 = 2;
+                break;
+        }
+
+        SetMusicParameters(musicLayer1, musicLayer2, musicLayer3);
 
     }
 
@@ -88,5 +127,12 @@ public class GameManager : MonoBehaviour
         yield return new WaitForSeconds(1f);
         _gameObjectPaz.SetActive(false);
         
+    }
+
+    public void SetMusicParameters(float musicLayer1, float musicLayer2, float musicLayer3)
+    {
+        _instanceFmod.setParameterByName("Gameplay Music 1 Layers", musicLayer1);
+        _instanceFmod.setParameterByName("Gameplay Music 2 Layers", musicLayer2);
+        _instanceFmod.setParameterByName("Parte Gameplay", musicLayer3);
     }
 }
